6465c21218bdb565a64e68c325067d4fa55e1654
Leonardo Staglianò <leonardo.stagliano@mail.polimi.it>
2019-06-20 13:51:36 +0200

    Caricamento un modello funzionante codice due modelli

--- drawone.js
+++ drawone.js
@@ -11,7 +11,29 @@ function main() {
     var cubeRy = 0.0;
     var cubeRz = 0.0;
     var cubeS  = 0.5;
-    var roadModel;
+    var roadModel = new Array();
+    var vaos = new Array();
+
+    var roadVertices = new Array();
+    var roadIndices = new Array();
+    var roadTextCoords = new Array();
+    var meshMatIndex = new Array();
+
+    var imageName = new Array();
+
+    var image = new Array();
+
+    var texture = new Array();
+
+    var world = new Array();
+
+    world[0] = utils.MakeWorld(0.0, 0.0, 0.0, cubeRx, 90, cubeRz, 1.0);
+    world[1] = utils.MakeWorld(0.0, 1.0, 0.0, cubeRx, 90, cubeRz, 1.0);
+
+
+    var UVFileNamePropertyIndex = new Array();
+    var diffuseColorPropertyIndex = new Array();
+    var specularColorPropertyIndex = new Array();
 
     var canvas = document.getElementById("c");
     var gl = canvas.getContext("webgl2");
@@ -19,6 +41,13 @@ function main() {
         document.write("GL context not opened");
         return;
     }
+    for(let i=0; i<2; i++){
+        vaos[i] = gl.createVertexArray();
+    }
+
+    var positionBuffer = gl.createBuffer();
+    var uvBuffer = gl.createBuffer();
+    var indexBuffer = gl.createBuffer();
     utils.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0.85, 0.85, 0.85, 1.0);
@@ -33,11 +62,14 @@ function main() {
     });
     gl.useProgram(program);
 
-    utils.get_json(baseDir + 'road01.json', function(loadedModel){roadModel = loadedModel;});
+    utils.get_json(baseDir + 'road02.json', function(loadedModel){roadModel[0] = loadedModel;});
+    utils.get_json(baseDir + 'road01.json', function(loadedModel){roadModel[1] = loadedModel;});
 
-    var susanVertices = roadModel.meshes[0].vertices;
-    var susanIndices = [].concat.apply([], roadModel.meshes[0].faces);
-    var susanTexCoords = roadModel.meshes[0].texturecoords[0];
+    for (let i=0; i<2; i++) {
+        roadVertices[i] = roadModel[i].meshes[0].vertices;
+        roadIndices[i] = [].concat.apply([], roadModel[i].meshes[0].faces);
+        roadTextCoords[i] = roadModel[i].meshes[0].texturecoords[0];
+    }
 
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
     var uvAttributeLocation = gl.getAttribLocation(program, "a_uv");
@@ -47,54 +79,43 @@ function main() {
     var perspectiveMatrix = utils.MakePerspective(90, gl.canvas.width/gl.canvas.height, 0.1, 100.0);
     var viewMatrix = utils.MakeView(1.5, 0.0, 3.0, 0.0, -30.0);
 
-    var vao = gl.createVertexArray();
-    gl.bindVertexArray(vao);
+    for(let i=0; i<2; i++){
 
-    var positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(susanVertices), gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+        console.log(roadVertices[i]);
 
-    var uvBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(susanTexCoords), gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(uvAttributeLocation);
-    gl.vertexAttribPointer(uvAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+        meshMatIndex[i] = roadModel[i].meshes[0].materialindex;
 
-    var indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(susanIndices), gl.STATIC_DRAW);
+        for (n = 0; n < roadModel[i].materials[meshMatIndex[i]].properties.length; n++){
+            if(roadModel[i].materials[meshMatIndex[i]].properties[n].key == "$tex.file") UVFileNamePropertyIndex[i] = n;
+            if(roadModel[i].materials[meshMatIndex[i]].properties[n].key == "$clr.diffuse") diffuseColorPropertyIndex[i] = n;
+            if(roadModel[i].materials[meshMatIndex[i]].properties[n].key == "$clr.specular") specularColorPropertyIndex[i] = n;
+        }
+
+        imageName[i] = roadModel[i].materials[meshMatIndex[i]].properties[UVFileNamePropertyIndex[i]].value;
 
 
-    var meshMatIndex = roadModel.meshes[0].materialindex;
 
-    var UVFileNamePropertyIndex = -1;
-    var diffuseColorPropertyIndex = -1;
-    var specularColorPropertyIndex = -1;
-    for (n = 0; n < roadModel.materials[meshMatIndex].properties.length; n++){
-        if(roadModel.materials[meshMatIndex].properties[n].key == "$tex.file") UVFileNamePropertyIndex = n;
-        if(roadModel.materials[meshMatIndex].properties[n].key == "$clr.diffuse") diffuseColorPropertyIndex = n;
-        if(roadModel.materials[meshMatIndex].properties[n].key == "$clr.specular") specularColorPropertyIndex = n;
+        texture[i] = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture[i]);
+
+        image[i] = new Image();
+        image[i].num = i;
+
+        requestCORSIfNotSameOrigin(image[i], baseDir + imageName);
+        image[i].src = baseDir + imageName[i];
+
+
+        image[i].onload= function() {
+            gl.bindTexture(gl.TEXTURE_2D, texture[this.num]);
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA,
+                gl.UNSIGNED_BYTE, new Uint8Array([255, 0, 0, 255]));
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.generateMipmap(gl.TEXTURE_2D);
+        };
+
     }
-
-    console.log(roadModel.materials[meshMatIndex].properties[UVFileNamePropertyIndex].value);
-    var imageName = roadModel.materials[meshMatIndex].properties[UVFileNamePropertyIndex].value;
-
-    var texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-
-    var image = new Image();
-    requestCORSIfNotSameOrigin(image, baseDir + imageName);
-    image.src = baseDir + imageName;
-    image.onload= function() {
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.generateMipmap(gl.TEXTURE_2D);
-    };
 
     drawScene();
 
@@ -102,33 +123,52 @@ function main() {
         var currentTime = (new Date).getTime();
         if(lastUpdateTime){
             var deltaC = (30 * (currentTime - lastUpdateTime)) / 1000.0;
-            cubeRx += deltaC;
-            cubeRy -= deltaC;
-            cubeRz += deltaC;
+            cubeRx += 0;
+            cubeRy -= 0;
+            cubeRz += 0;
         }
-        worldMatrix = utils.MakeWorld(0.0, 0.0, 0.0, cubeRx, cubeRy, cubeRz, 1.0);
+
+        worldMatrix = utils.MakeWorld(0.0, 0.0, 0.0, cubeRx, 67, cubeRz, 1.0);
+
         lastUpdateTime = currentTime;
     }
 
     function drawScene() {
-        animate();
+        for (let i=0; i<2; i++) {
+            animate();
 
-        utils.resizeCanvasToDisplaySize(gl.canvas);
-        gl.clearColor(0.85, 0.85, 0.85, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            utils.resizeCanvasToDisplaySize(gl.canvas);
+            gl.clearColor(0.85, 0.85, 0.85, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        var viewWorldMatrix = utils.multiplyMatrices(viewMatrix, worldMatrix);
-        var projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, viewWorldMatrix);
+            var viewWorldMatrix = utils.multiplyMatrices(viewMatrix, world[i]);
+            var projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, viewWorldMatrix);
 
-        gl.uniformMatrix4fv(matrixLocation, gl.FALSE, utils.transposeMatrix(projectionMatrix));
+            gl.uniformMatrix4fv(matrixLocation, gl.FALSE, utils.transposeMatrix(projectionMatrix));
 
-        gl.activeTexture(gl.TEXTURE0);
-        gl.uniform1i(textLocation, texture);
 
-        gl.bindVertexArray(vao);
-        gl.drawElements(gl.TRIANGLES, susanIndices.length, gl.UNSIGNED_SHORT, 0 );
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(roadVertices[i]), gl.STATIC_DRAW);
+            gl.enableVertexAttribArray(positionAttributeLocation);
+            gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(roadTextCoords[i]), gl.STATIC_DRAW);
+            gl.enableVertexAttribArray(uvAttributeLocation);
+            gl.vertexAttribPointer(uvAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(roadIndices[i]), gl.STATIC_DRAW);
+
+            gl.activeTexture(gl.TEXTURE0);
+            gl.uniform1i(textLocation, texture[i]);
+
+            gl.drawElements(gl.TRIANGLES, roadIndices[i].length, gl.UNSIGNED_SHORT, 0);
+        }
 
         window.requestAnimationFrame(drawScene);
+
     }
 }
 
