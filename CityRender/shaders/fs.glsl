993df707bb2e2ef7b44bb8f737373b8b0ab71d08
mauritimo <10503085@polimi.it>
2019-07-03 00:28:03 +0200

    Added pedestrian crossing trasparency

--- CityRender/shaders/fs.glsl
+++ CityRender/shaders/fs.glsl
@@ -10,13 +10,8 @@ uniform sampler2D u_texture;
 
 
 vec3 lightPos= vec3(0.0f,7.0f,10.0f);
-
-
 vec3 lightColor = vec3(2.3f,2.1f,2.1f);
-
-
 vec3 objectColor = vec3(0.4f,0.4f,0.4f);
-
 vec3 FragPos = vec3(1.0f,1.0f,1.0f);
 
 
@@ -32,5 +27,8 @@ void main() {
     vec3 diffuse = diff * lightColor ;
 
     vec3 result =  (ambient + diffuse) * objectColor;
-    outColor = vec4(result, 1.0) * texture(u_texture, uvFS) ;
+    vec4 texColor= texture(u_texture, uvFS);
+    if(texColor.a < 0.5)  // cut to 0.5 considering objects are only visible (1.0) or invisible (0.0) 
+        discard;
+    outColor = vec4(result, 1.0) * texColor;
 }
