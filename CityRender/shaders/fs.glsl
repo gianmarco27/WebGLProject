e12df6e89fc69e040d597edb0391ad4c42ecaa54
mauritimo <10503085@polimi.it>
2019-07-03 16:53:25 +0200

    Added point light

--- CityRender/shaders/fs.glsl
+++ CityRender/shaders/fs.glsl
@@ -5,30 +5,74 @@ precision mediump float;
 
 in vec2 uvFS;
 in vec3 Normal;
+in vec3 lightDir;
 out vec4 outColor;
 uniform sampler2D u_texture;
 
 
-vec3 lightPos= vec3(0.0f,7.0f,10.0f);
-vec3 lightColor = vec3(2.3f,2.1f,2.1f);
-vec3 objectColor = vec3(0.4f,0.4f,0.4f);
-vec3 FragPos = vec3(1.0f,1.0f,1.0f);
+//vec3 lightPos= vec3(100.0f,7.0f,10.0f);
+vec3 lightColor = vec3(2.0f,2.0f,2.1f);
+vec3 objectColor = vec3(1.0f,1.0f,1.0f);
+//vec3 FragPos = vec3(1.0f,1.0f,1.0f);
+
+
+
+//distance = 13
+float constant = 1.0;
+float linear = 0.35;
+float quadratic = 0.44;
 
 
 void main() {
 
-    float ambientStrength = 0.5;
+    float ambientStrength = 0.7;
     vec3 ambient = ambientStrength * lightColor;
-
-
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = dot(norm, lightDir);
+    vec3 lightDirNorm = normalize(lightDir);
+    float diff = max(dot(norm, lightDirNorm),0.0);
     vec3 diffuse = diff * lightColor ;
+    
+    
+   float distance = length(lightDir);
+   float attenuation = 1.0 / (constant + linear * distance + 
+    		    quadratic * (distance * distance));    
+                
+   ambient  *= attenuation; 
+   diffuse  *= attenuation;
+    
 
     vec3 result =  (ambient + diffuse) * objectColor;
     vec4 texColor= texture(u_texture, uvFS);
     if(texColor.a < 0.5)  // cut to 0.5 considering objects are only visible (1.0) or invisible (0.0) 
         discard;
-    outColor = vec4(result, 1.0) * texColor;
+    outColor = vec4(result, 1.0)* texColor;
 }
+
+
+
+/* distance = 7
+float constant = 1.0;
+float linear = 0.7;
+float quadratic = 1.8;
+
+*/
+
+/* distance = 20
+float constant = 1.0;
+float linear = 0.22;
+float quadratic = 0.20;
+*/
+
+
+/* distance = 32
+float constant = 1.0;
+float linear = 0.14;
+float quadratic = 0.07;
+*/
+
+
+/* distance=200
+float constant = 1.0;
+float linear = 0.022;
+float quadratic = 0.0019;
+*/
--- CityRender/shaders/vs.glsl
+++ CityRender/shaders/vs.glsl
@@ -6,12 +6,56 @@ in vec3 a_normal;
 out vec2 uvFS;
 out vec3 Normal;
 out vec3 FragPos;
+out vec3 lightDir;
 
 uniform mat4 matrix;
 uniform mat4 model;
+vec3 lightPos = vec3(0,4,1);
 void main() {
   uvFS = a_uv;
   gl_Position = matrix * vec4(a_position,1.0);
   FragPos= vec3(model* vec4(a_position, 1.0));
-  Normal = a_normal; ;
+  lightDir = lightPos - FragPos;
+  Normal = a_normal; 
 }
+
+
+
+
+
+
+/*
+
+float constant = 1.0f;
+float linear = 0.09;
+float quadratic = 0.032;
+
+*/
+
+
+
+
+/*
+#version 300 es
+
+in vec4 a_position;
+in vec2 a_uv;
+in vec3 a_normal;
+out vec2 uvFS;
+out vec3 Normal;
+out vec3 v_surfaceToLight;
+uniform mat4 matrix;
+uniform mat4 model;  //(u_world)
+vec3 u_lightWorldPosition= vec3(10,10,10);
+
+vec3 FragPos;
+
+
+void main() {
+  uvFS = a_uv;
+  gl_Position = matrix * a_position;
+  vec3 FragPos= (model * a_position).xyz;
+  v_surfaceToLight = u_lightWorldPosition - FragPos;
+  Normal = a_normal; 
+}
+*/
